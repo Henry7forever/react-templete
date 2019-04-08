2e4b98df45683b3f81a9c9ba2f88c2246b548b46
Andy Yeh <heroandy4@gmail.com>
2019-04-08 10:56:00 +0800

    Updates

---
+++ precache-manifest.86b864bb4641f65bcb770d72e3351e4e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f4521d4d0748b8efe391",
+    "url": "/react-templete/static/css/main.4825e076.chunk.css"
+  },
+  {
+    "revision": "f4521d4d0748b8efe391",
+    "url": "/react-templete/static/js/main.9b7988b9.chunk.js"
+  },
+  {
+    "revision": "74c194e2e39b329bd391",
+    "url": "/react-templete/static/js/runtime~main.9c8a4267.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/react-templete/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-templete/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "aea8161b20dd2544eb55c5f05542c1cf",
+    "url": "/react-templete/index.html"
+  }
+];
